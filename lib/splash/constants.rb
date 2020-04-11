f66e3892a31dd9d0bbe3ab72e4ed1728cb85688c
Romain Georges <romain@ultragreen.net>
2020-04-11 19:54:13 +0200

    REL 0.2.0

--- lib/splash/constants.rb
+++ lib/splash/constants.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 module Splash
   module Constants
-    VERSION = "0.1.1"
+    VERSION = "0.2.0"
 
     # the path to th config file, not overridable by config
     CONFIG_FILE = "/etc/splash.yml"

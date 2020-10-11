95e7c72d529f4c58fce8f293d6c8797c3a464ae7
Romain Georges <romain@ultragreen.net>
2020-10-11 21:10:38 +0200

    RELEASE 0.6.1

--- lib/splash/constants.rb
+++ lib/splash/constants.rb
@@ -7,7 +7,7 @@ module Splash
   module Constants
 
     # Current splash version
-    VERSION = "0.6.0"
+    VERSION = "0.6.1"
 
     # the path to th config file, not overridable by config
     CONFIG_FILE = "/etc/splash.yml"
